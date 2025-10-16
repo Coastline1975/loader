32efa2f9f469bfdcf007ce0adf7b915fa27345ff
hfjghhfhghfhjgjghfhffgghhjghfgfjgfgjhfhj <74459442+Coastline1975@users.noreply.github.com>
2025-10-16 12:11:07 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 301549746 then -- Counter Blox
-    print("")
+    loadstring("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon")()
 elseif game.PlaceId == 286090429 then -- Arsenal
-    print("")
-else
-    print("")
+    loadstring("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/ArsenalOPWeapons")()
+elseif game.PlaceId == 11 then -- Arsenal
+    loadstring("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon")()
 end
