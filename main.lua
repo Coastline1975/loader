1c7e73fd9de41ba787040f9bc75a17dc2ef49676
hfjghhfhghfhjgjghfhffgghhjghfgfjgfgjhfhj <74459442+Coastline1975@users.noreply.github.com>
2025-10-16 15:12:01 +0000

    Add MM2 script loading for PlaceId 142823291
    Added support for MM2 autofarm script based on PlaceId.

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ if game.PlaceId == 301549746 then -- Counter Blox
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon"))()
 elseif game.PlaceId == 286090429 then -- Arsenal
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/ArsenalOPWeapons"))()
-elseif game.PlaceId == 11 then -- Arsenal
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon"))()
+elseif game.PlaceId == 142823291 then -- MM2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/MM2Autofarm"))()
+elseif game.PlaceId == 1 then
 end
