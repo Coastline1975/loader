315d065cab49746132305ead5521a1ec61d40012
hfjghhfhghfhjgjghfhffgghhjghfgfjgfgjhfhj <74459442+Coastline1975@users.noreply.github.com>
2025-10-16 12:15:00 +0000

    Replace loadstring with game:HttpGet for scripts

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 301549746 then -- Counter Blox
-    loadstring("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon"))()
 elseif game.PlaceId == 286090429 then -- Arsenal
-    loadstring("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/ArsenalOPWeapons")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/ArsenalOPWeapons"))()
 elseif game.PlaceId == 11 then -- Arsenal
-    loadstring("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/loader/refs/heads/main/scripts/CounterBloxModifyWeapon"))()
 end
